a71d91460b736da30d95293d70c89bcb1d6ae146
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-03-28 11:05:16 -0300

    Solução lição

--- main.js
+++ main.js
@@ -2,10 +2,13 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
+            description: 'Confortáveis e unissex.',
             image: './assets/images/socks_green.jpg',
             // solution
-            url: 'https://www.vuemastery.com/'
+            url: 'https://www.vuemastery.com/',
             // solution
+            inventory: 0,
+            onSale: false
         }
     }
 })
