4d5731daf0584dddd0ce918532e88faa8012cc9b
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-03-28 14:01:02 -0300

    Computed properties

--- main.js
+++ main.js
@@ -2,16 +2,15 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
-            description: 'Confortáveis e unissex.',
-            image: './assets/images/socks_green.jpg',
+            brand: 'Vue Mastery',
+            description: 'Comfortable e unisex.',
+            selectedVariant: 0,
             url: 'https://www.vuemastery.com/',
-            inventory: 3,
-            inStock: false,
-            onSale: false,
+            onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 8 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
             ],
             sizes: ['S', 'M', 'L', "XL"],
             cart: 0
@@ -28,6 +27,27 @@ const app = Vue.createApp({
         },
         updateImage(variantImage) {
             this.image = variantImage;
+        },
+        updateVariant(index) {
+            this.selectedVariant = index
+            console.log(index)
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        sale() {
+            if (this.onSale) {
+                return this.title + ' is on sale.'
+            }
+            return ''
         }
     }
 })
