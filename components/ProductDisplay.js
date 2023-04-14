05def60929c9d3c8c1bc7ebf4ff9d22b7f5b4c96
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-04-14 20:34:35 -0300

    Indentação.

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -1,112 +1,112 @@
 application.component('product-display', {
-    props: {
-        premium: {
-            type: Boolean,
-            required: true
-        }
-    },
-    template:
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
+  template:
     /*html*/
     `
     <div class="product-display">
-    <div class="product-container">
-      <div class="product-image">
-        <img 
-          v-bind:class="{ 'out-of-stock-img': !inStock }"
-          v-bind:src="image" 
-          :alt="description">
-      </div>
-      <div class="product-info">
-        <h1>{{ title }}</h1>
-        <p>{{ description }}</p>
-        <a :href="url">Made by Vue Mastery</a>
-        <p v-if="inStock > 10">In Stock</p>
-        <p v-else-if="inStock > 0 && inStock <= 10">Almost Sold Out</p>
-        <p v-else>Out of Stock</p>
-        <p>Shipping: {{ shipping }}</p>
-        <p>{{ sale }}</p>
-        <product-details :details="details"></product-details>
-        <ul>
-          <li v-for="(size, index) in sizes" :key="index">{{ size }}</li>
-        </ul>
-        <div
-          v-for="(variant, index) in variants"
-          :key="variant.id" 
-          @mouseover="updateVariant(index)"
-          class="color-circle"
-          :style="{ backgroundColor: variant.color }">
+      <div class="product-container">
+        <div class="product-image">
+          <img 
+              v-bind:class="{ 'out-of-stock-img': !inStock }"
+              v-bind:src="image" 
+              :alt="description">
         </div>
-        <button 
-          class="button" 
-          @click="addToCart"
-          :class="{ disabledButton: !inStock }"
-          :disabled="!inStock">
-          Add to Cart
-        </button>
-        <button
-          class="button"
-          @click="removeFromCart"
-          :class="{ disabledButton: !inStock }" 
-          :disabled="!inStock">
-          Remove Item
-        </button>
+        <div class="product-info">
+          <h1>{{ title }}</h1>
+          <p>{{ description }}</p>
+          <a :href="url">Made by Vue Mastery</a>
+          <p v-if="inStock > 10">In Stock</p>
+          <p v-else-if="inStock > 0 && inStock <= 10">Almost Sold Out</p>
+          <p v-else>Out of Stock</p>
+          <p>Shipping: {{ shipping }}</p>
+          <p>{{ sale }}</p>
+              <product-details :details="details"></product-details>
+          <ul>
+              <li v-for="(size, index) in sizes" :key="index">{{ size }}</li>
+          </ul>
+          <div
+              v-for="(variant, index) in variants"
+              :key="variant.id" 
+              @mouseover="updateVariant(index)"
+              class="color-circle"
+              :style="{ backgroundColor: variant.color }">
+          </div>
+          <button 
+              class="button" 
+              @click="addToCart"
+              :class="{ disabledButton: !inStock }"
+              :disabled="!inStock">
+              Add to Cart
+          </button>
+          <button
+              class="button"
+              @click="removeFromCart"
+              :class="{ disabledButton: !inStock }" 
+              :disabled="!inStock">
+              Remove Item
+          </button>
+        </div>
       </div>
-    </div>
     </div>
     `,
-    data() {
-        return {
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            description: 'Comfortable e unisex.',
-            details: ['50% cotton', '30% wool (teste)', '20% polyester'],
-            selectedVariant: 0,
-            url: 'https://www.vuemastery.com/',
-            onSale: true,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 8 },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
-            ],
-            sizes: ['S', 'M', 'L', 'XL']
-        }
+  data() {
+    return {
+      product: 'Socks',
+      brand: 'Vue Mastery',
+      description: 'Comfortable e unisex.',
+      details: ['50% cotton', '30% wool (teste)', '20% polyester'],
+      selectedVariant: 0,
+      url: 'https://www.vuemastery.com/',
+      onSale: true,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 8 },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
+      ],
+      sizes: ['S', 'M', 'L', 'XL']
+    }
+  },
+  methods: {
+    addToCart() {
+      this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
     },
-    methods: {
-        addToCart() {
-            this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
-        },
-        removeFromCart() {
-            this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
-        },
-        updateImage(variantImage) {
-            this.image = variantImage;
-        },
-        updateVariant(index) {
-            this.selectedVariant = index
-            console.log(index)
-        }
+    removeFromCart() {
+      this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
     },
-    computed: {
-        title() {
-            return this.brand + ' ' + this.product
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
-        },
-        sale() {
-            if (this.onSale) {
-                return this.title + ' is on sale.'
-            }
-            return ''
-        },
-        shipping() {
-            if (this.premium) {
-                return 'Free'
-            }
-            return 2.99
-        }
+    updateImage(variantImage) {
+      this.image = variantImage;
     },
+    updateVariant(index) {
+      this.selectedVariant = index
+      console.log(index)
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product
+    },
+    image() {
+      return this.variants[this.selectedVariant].image
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity
+    },
+    sale() {
+      if (this.onSale) {
+        return this.title + ' is on sale.'
+      }
+      return ''
+    },
+    shipping() {
+      if (this.premium) {
+        return 'Free'
+      }
+      return 2.99
+    }
+  },
 })
